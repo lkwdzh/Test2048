6177d9632768daf313da2d16dd94ec3146dd396a
lkwdzh <lkwdxyx@163.com>
2016-04-06 16:53:07 +0800

    2048学习

---
+++ app/src/main/java/com/aglook/test2048/Grid.java
@@ -0,0 +1,11 @@
+package com.aglook.test2048;
+
+/**
+ * Created by lkw on 2016/4/6.
+ */
+public class Grid {
+    public Tile[][] field;
+    public Tile[][] undoField;
+    private Tile[][] bufferField;
+
+}
